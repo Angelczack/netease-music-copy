355fcf583f027647d8b121eac6dc10e363f5eed8
Angelczack <2992138143@qq.com>
2023-05-25 13:46:27 +0800

    netease

---
+++ musicobj02/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div id="d1">
+      oooooooo
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#d1{
+  width: 3rem;
+  height: 3rem;
+  background: skyblue;
+  font-size: 0.3rem;
+}
+</style>
+
